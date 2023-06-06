3322ccd290c55a6ca3bbe5c71228d294aa1aa91b
bartlomiejOl <bartek.olek.praca@gmail.com>
2023-06-06 13:31:32 +0200

    Zaktualizowany plik server.js z konfiguracją CORS

--- server/index.js
+++ server/index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { mongoose } = require('mongoose');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 
-//  database connection 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Pomyślnie połączono z bazą danych'))
-.catch((err) => console.log('Błąd połączenia z bazą danych', err))
+//  database connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Pomyślnie połączono z bazą danych'))
+  .catch((err) => console.log('Błąd połączenia z bazą danych', err));
 
 // middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', require('./routes/authRoutes'))
+app.use('/', require('./routes/authRoutes'));
 
 const port = 8000;
-app.listen(port, ()=> console.log(`Server działa na porcie ${port}`))
+app.listen(port, () => console.log(`Server działa na porcie ${port}`));
