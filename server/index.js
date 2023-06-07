3e71bc7f18068677b7585f9dfb2871f5bf997ed3
bartlomiejOl <bartek.olek.praca@gmail.com>
2023-06-07 13:51:33 +0200

    Zaktualizowany server

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   next();
 });
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
